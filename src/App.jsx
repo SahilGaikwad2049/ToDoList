3866074eb986cb7acac6cb7ac494fb911dce59d3
SahilGaikwad2049 <gaikwadsahil1211@gmail.com>
2024-07-23 20:43:36 +0530

    commit two

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ function App() {
   return (
     <>
       <Navbar/>
+      <div className="container mx-auto bg-cyan-500 my-5 text-white rounded-xl p-5 min-h-[80vh]">
+        <div className="add">
+          <h2 className='text-xl font-extrabold'>Add a Task</h2>
+          <input type="text" />
+          <button className='bg-blue-700 p-3 font-bold rounded-md py-1 mx-6 text-sm'>Add</button>
+        </div>
+        <h2 className='text-xl font-bold'>ToDo's</h2>
+        <div className='todo flex'>
+          <div className="text">Lorem ipsum dolor sit amet consectetur.</div>
+          <div className="buttons">
+          <button className='bg-blue-700 p-3 font-bold rounded-md py-1 mx-6 text-sm'>Add</button>
+          <button className='bg-blue-700 p-3 font-bold rounded-md py-1 mx-6 text-sm'>Add</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+    <nav className='flex justify-between bg-pink-500 text-white py-3'>
+        <span className='font-bold text-xl mx-6'>ChutFinder</span>
+        <ul className='flex gap-4 mx-6'>
+            <li className='cursor-pointer'>Home</li>
+            <li className='cursor-pointer'>Tasks</li>
         </ul>
     </nav>
   )
